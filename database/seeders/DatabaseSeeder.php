08892c8f2aa138976301ddb407776c0f68747e06
SiMohamed10 <mohamedelbouatmani@gmail.com>
2023-05-18 20:12:46 +0100

    Final Touches
    Les touches finales pour le projet . Seeders et Factory et images ( logo )

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,17 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Enums\Roles;
+use App\Models\Job;
+use App\Models\Profile;
+use App\Models\Skill;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +23,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(40)->create();
+
+        User::create([
+            "name" => "mohamed",
+            "email" => "mohamed@gmail.com",
+            "password" => Hash::make("helloworld")
+        ]);
+
+
+        Skill::factory(50)->create();
+
+
+        foreach (User::where("id", ">=", 2)->get() as $user) {
+            $user->assignRole(Roles::ENTREPRENEUR->value);
+        }
+
+        Job::factory(6000)->create();
+
+        foreach (Job::all() as $job) {
+            foreach (Skill::inRandomOrder()->take(5)->get() as $skill) {
+                $job->skills()->attach($skill->id);
+            }
+        }
+
+        Profile::factory(40)->create();
     }
 }
